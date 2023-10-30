1ed76cd2b280c18d55e41098e2ed3af2e3d5d5a8
KedarCoder <144768574+KedarCoder@users.noreply.github.com>
2023-10-30 17:47:55 +0530

    Delete searching-filter_with_db directory

--- searching-filter_with_db/src/main/java/com/concerto/searching/filter/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.concerto.searching.filter.repository;
-
-import org.springframework.stereotype.Repository;
-
-import com.concerto.searching.filter.bean.Item;
-
-@Repository
-public interface ItemRepository extends GenericRepository<Item, Long> {	
-}
